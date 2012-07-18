7c053f974d028ded7de0af586d34f32941efd2cf
Edward Kim <eddotman@gmail.com>
2012-07-18 11:29:01 -0400

    added some basic input/output functions

--- main.cpp
+++ main.cpp
@@ -8,6 +8,11 @@
 
 using namespace std;
 
+//Error codes
+const int NO_ERR = 0;
+const int EXIT_CMD = -1;
+const int BAD_INPUT = 1;
+
 class Sample
 {
     private:
@@ -30,32 +35,119 @@ class Sample
 
     Sample(string name);
     int compute(); //Compute xray properties
-    int dilute(string compound) //Dilute sample using a specified compound
-    int rename(string name); //Change sample name
+    int dilute(string compound); //Dilute sample using a specified compound
+    int rename(string sample_name); //Change sample name
     int write_screen(); //Write sample data to screen
     int write_file(string file_name); //Write sample data to file
+
+    string get_name();
 };
 
-int parse_input(string input)
+Sample::Sample(string sample_name)
 {
-    return 0;
+    name = sample_name;
+}
+
+string Sample::get_name()
+{
+    return name;
+}
+
+void string_explode(string str, string separator, vector<string>* results){
+    unsigned int found;
+    found = str.find_first_of(separator);
+    while(found != string::npos){
+        if(found > 0){
+            results->push_back(str.substr(0,found));
+        }
+        str = str.substr(found+1);
+        found = str.find_first_of(separator);
+    }
+    if(str.length() > 0){
+        results->push_back(str);
+    }
+}
+
+//Samples
+vector < Sample > samples;
+
+int parse_input(string mode = "none")
+{
+    string user_input;
+    vector < string > filtered_input;
+
+    cout << "Please enter a command:" << endl;
+    cout << ">>>";
+    getline(cin, user_input);
+    cout << endl;
+
+    string_explode(user_input, " ", &filtered_input); //Explode the string by each word
+
+    //Quit Program
+    if (filtered_input[0] == "quit")
+    {
+        return EXIT_CMD;
+    }
+    //Sample commands
+    else if (filtered_input[0] == "sample")
+    {
+        if (filtered_input[1] == "new")
+        {
+            if (filtered_input.size() == 3)
+            {
+                samples.push_back(Sample(filtered_input[2]));
+            }
+            else
+            {
+                cout << "A one-word sample name is required. Please re-input." << endl;
+            }
+        }
+        else
+        {
+            cout << "Bad subcommand under command 'sample' -- Please re-input." << endl;
+        }
+    }
+    //List samples
+    else if (filtered_input[0] == "list")
+    {
+
+        if (samples.size() != 0)
+        {
+            cout << "List of samples:" << endl;
+
+            for (unsigned int i = 0; i < samples.size(); i++)
+            {
+                cout << (i+1) << ". " << samples[i].get_name() << endl;
+            }
+        }
+        else
+        {
+            cout << "No samples to list." << endl;
+        }
+    }
+    //Bad command name
+    else
+    {
+        cout << "Bad command name. Please re-input." << endl;
+    }
+
+    cout << endl;
+    return NO_ERR;
 }
 
 int main()
 {
-    const int NO_ERR = 0;
-    const int EXIT_CMD = -1;
-
-    string user_input;
     int err = NO_ERR;
+
+    //Welcome message
+    cout << "Welcome to the XAFS Sample Prep Calculator" << endl;
+    cout << "Type 'help' for help and 'quit' to quit the program" << endl;
+    cout << "Created by Eddie Kim, July 2012" << endl << endl;
 
     //Input loop
     do
     {
-        cout << "Please enter a command:" << endl;
-        cin >> user_input;
-
-        err = parse_input(user_input);
+        err = parse_input();
 
     }while(err != EXIT_CMD);
 
